646f1cb158a8eacbde2de7247bf61c4aad34f562
urmelodr10 <dungrugthai.othong@gmail.com>
2023-02-14 12:06:38 +0700

    c12

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+//======login  register=============
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose =
+        require("passport-local-mongoose"),
+    User = require("./models/user");
+ 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/auth_demo_app");
+ 
+var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.use(require("express-session")({
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false
+}));
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
 var express = require('express');
 var app = express();
 var path = require('path');
