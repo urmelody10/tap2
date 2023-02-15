99d16bb116b308fc853284b9ddd01c9af3f405a7
urmelodr10 <dungrugthai.othong@gmail.com>
2023-02-15 10:28:47 +0700

    c20

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
     name: String,
-    mood: String,
+    mood: Array,
     assessment: String,
     priority: String,
 
